f8a27b64a92e4cf607a171b72eb42d0311aaf27c
psotose <p.sotosellanes@hotmail.com>
2022-07-09 18:27:23 -0400

    add try/catch

--- src/services/ipifyApi.js
+++ src/services/ipifyApi.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
 const ipifyApi = async (state) => {
+try {  
   const petition = await axios.get('https://api.ipify.org/?format=json')
   state(petition.data.ip);
+} catch (e) {
+  state(null)
+  console.log("error", e);
+} finally {
+  console.log("process done");
+}
 }
 
 export default ipifyApi;
